4b608171fb35cc2deb9c6a1a68ea96057b14ac8d
Ryan Wilson <ryan.wilson0198@gmail.com>
2022-11-02 17:18:32 +0000

    add divorce link for desktop only

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, NavLink, NavItem, NavbarToggler, Collapse, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 var navStyle = {
     backgroundColor: "#D7D9D8",
@@ -63,22 +64,43 @@ export class Navigation extends React.Component {
                         <NavItem>
                             <NavLink href="/">Home</NavLink>
                         </NavItem>
-                        <Dropdown nav inNavbar onMouseOver={this.onMouseOverDropdown} onMouseLeave={this.onMouseLeaveDropdown} isOpen={this.state.isOpenDropdown} toggle={this.dropdownToggle} onClick={this.divorceOnClick}>
-                            <DropdownToggle nav>
-                                Divorce
-                            </DropdownToggle>
-                            <DropdownMenu end style={dropdownStyle}>
-                                <DropdownItem href="/divorce/resources">
-                                    Resources
-                                </DropdownItem>
-                                <DropdownItem href="/divorce/member-stories">
-                                    Member Stories
-                                </DropdownItem>
-                                <DropdownItem href="/divorce/questionnaire">
-                                    Questionnaire
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
+                        <BrowserView>
+                            <Dropdown nav inNavbar onMouseOver={this.onMouseOverDropdown} onMouseLeave={this.onMouseLeaveDropdown} isOpen={this.state.isOpenDropdown} toggle={this.dropdownToggle} onClick={this.divorceOnClick}>
+                                <DropdownToggle nav>
+                                    Divorce
+                                </DropdownToggle>
+                                <DropdownMenu end style={dropdownStyle}>
+                                    <DropdownItem href="/divorce/resources">
+                                        Resources
+                                    </DropdownItem>
+                                    <DropdownItem href="/divorce/member-stories">
+                                        Member Stories
+                                    </DropdownItem>
+                                    <DropdownItem href="/divorce/questionnaire">
+                                        Questionnaire
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </BrowserView>
+                        <MobileView>
+                            <Dropdown nav inNavbar onMouseOver={this.onMouseOverDropdown} onMouseLeave={this.onMouseLeaveDropdown} isOpen={this.state.isOpenDropdown} toggle={this.dropdownToggle}>
+                                <DropdownToggle nav>
+                                    Divorce
+                                </DropdownToggle>
+                                <DropdownMenu end style={dropdownStyle}>
+                                    <DropdownItem href="/divorce/resources">
+                                        Resources
+                                    </DropdownItem>
+                                    <DropdownItem href="/divorce/member-stories">
+                                        Member Stories
+                                    </DropdownItem>
+                                    <DropdownItem href="/divorce/questionnaire">
+                                        Questionnaire
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </MobileView>
+                        
                         <NavItem>
                             <NavLink href="/about">About</NavLink>
                         </NavItem>
